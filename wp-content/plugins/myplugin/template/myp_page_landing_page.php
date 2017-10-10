c7497b3ba56b2e738ebd05d2311db82a53383a80
es021 <zulsarhan.shaari@gmail.com>
2017-10-10 21:39:11 +0800

    Add tentative to landing page

--- wp-content/plugins/myplugin/template/myp_page_landing_page.php
+++ wp-content/plugins/myplugin/template/myp_page_landing_page.php
@@ -12,7 +12,7 @@
                 <div class='intro-subtext'>
                     <div id = "countdown"></div>
                     <small>October 11th - 15th, 2017 (8 pm - 12 am EST)</small>    
-                  
+
                     <script>
                         eventCountdown("countdown", '10/11/2017 08:00 PM EST', "x-large", "", "Queue is now open! Goodluck!");
                     </script>
@@ -48,39 +48,63 @@
         </section>
     <?php else: ?>
 
-                                        <!--        <section class="focus" id="focus">
-                                                </section>-->
-
         <style>
             .full_width{
-                width: 100vw;
-                margin-left: -15px;
+                width: 100%;
                 padding-top: 30px;
             }
+
+            .header-content-wrap{
+                padding-bottom: 80px;
+            }
+
+            .full_width.bg_trans{
+                background-color: rgba(255,255,255,0.85);
+            }
+
+            .full_width.bg_smoke{
+                background-color: whitesmoke;
+            }
+
+            @media only screen and (max-width: 360px){
+                .wzs21_fixed_image{
+                    width: auto;
+                    height: auto;
+                    max-width:100%;
+                }
+            }
+
         </style>
 
-        <div class="container-fluid">
-            <div class="row full_width" style='background-color: whitesmoke;'>
-                <div class="col-sm-12">
+        <div class="container-fluid no_padding">
+            <div class="row full_width bg_smoke" style="padding-bottom: 40px;">
+                <div class="col-sm-12 sm_no_padding text-center">
+                    <h3 class="dark-text gold-text">What To Expect?</h3>
+                    <?= generateFixImage(site_url() . "/image/content/tentative.jpeg", 500, 360) ?>
+                </div>
+            </div>
+
+            <div class="row full_width bg_trans" >
+                <div class="col-sm-12 item_fw">
                     <h3 class="dark-text gold-text">Platinum Sponsor</h3>
                     <?= generateFixImage(site_url() . "/image/client/EM.png", 200, 200) ?>
                 </div>
-                <div class="col-sm-12">
+                <div class="col-sm-12 item_fw">
                     <h3 class="dark-text gold-text">Gold Sponsors</h3>
                     <?= generateFixImage(site_url() . "/image/company/exxon_1.png", 200, 200) ?>
                     <?= generateFixImage(site_url() . "/image/company/shell.png", 200, 200) ?>
                     <?= generateFixImage(site_url() . "/image/company/sapura.png", 200, 200) ?>
                 </div>
-                <div class="col-sm-12">
+                <div class="col-sm-12 item_fw">
                     <h3 class="dark-text gold-text">Silver Sponsor</h3>
                     <?= generateFixImage(site_url() . "/image/company/axiata_1.png", 200, 200) ?>
                 </div>
-                <div class="col-sm-12">
+                <div class="col-sm-12 item_fw">
                     <h3 class="dark-text gold-text">Bronze Sponsor</h3>
                     <?= generateFixImage(site_url() . "/image/company/maybank.png", 120, 250) ?>
                     <br><br>
                 </div>
-                <div class="col-sm-12">
+                <div class="col-sm-12 item_fw">
                     <h3 class="dark-text gold-text">Participating Companies</h3>
                     <?= generateFixImage(site_url() . "/image/company/tfm.png", 125, 175) ?>
                     <?= generateFixImage(site_url() . "/image/company/aig.png", 125, 175) ?>
@@ -91,15 +115,13 @@
                 </div>
             </div>
 
-
-
-            <div class="row full_width" style='background-color: rgba(255,255,255,0.80);'>
-                <div class="col-sm-12">
+            <div class="row full_width bg_smoke">
+                <div class="col-sm-12 item_fw">
                     <h3 class="dark-text">Organized By</h3>
                     <?= generateFixImage(site_url() . "/image/client/NAMSA.png", 150, 150) ?>
                     <br><br>
                 </div>
-                <div class="col-sm-12">
+                <div class="col-sm-12 item_fw">
                     <h3 class="dark-text">In Collaboration With</h3>
                     <?= generateFixImage(site_url() . "/image/client/EPIC.png", 150, 150) ?>
                     <?= generateFixImage(site_url() . "/image/client/WCC.png", 150, 150) ?>
@@ -112,7 +134,7 @@
 
                 <hr class="line">
                 <h3 class="dark-text">Brought To You By</h3>
-                <div class="col-sm-12">
+                <div class="col-sm-12 item_fw">
                     <?= generateFixImage(site_url() . "/image/innovaseed.png", 110, 200) ?>
                 </div>
             </div>
